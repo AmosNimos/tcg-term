6215716516b7bf0f3c507044dbae4b7d2e36385a
Amos Nimos <76786560+AmosNimos@users.noreply.github.com>
2021-07-25 19:54:04 -0400

    Create gen_card.py

---
+++ gen_card.py
@@ -0,0 +1,37 @@
+from random import choice
+
+## Cards class
+class Creature:
+	def __init__(self, power, taughness, cost, card_kind):
+		self.power = power;
+		self.taughness = taughness;
+		self.kind = card_kind; # Human, Dinosaur Avatar, Vampire...
+		self.cost = cost;
+		self.total_cost = total_cost;
+		self.name = namegenerator.gen();
+		self.taped = False;
+
+class Land()
+		
+		color = ""
+		name = ""
+		supertype = "Basic"
+		rarity
+
+
+		# I need this variable for mana/plane.
+		def __init__(self, color ):
+			self.color = color;
+
+		def gen_color():
+			# the none cololor is for colorless mana.
+			color_id = randint(0,5)
+			colors=["none","basic","white","blue","red","green"]
+			name=["Wastes","Plains","Island","Swamp","Mountain","Forest"]
+
+			self.Name
+
+
+
+
+## Gem fimction
