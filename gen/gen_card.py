431852bf2db9a468c23a621cc734543e2df32a47
Amos Nimos <76786560+AmosNimos@users.noreply.github.com>
2021-07-29 20:00:49 -0400

    Add files via upload
    Gen player with more modularity.

---
+++ gen/gen_card.py
@@ -0,0 +1,39 @@
+from random import choice
+
+## Cards class
+class Creature:
+	def __init__(self, power, taughness, cost, card_kind):
+		self.power = power;
+		self.taughness = taughness;
+		self.kind = card_kind; # Human, Dinosaur Avatar, Vampire...
+		self.cost = cost;
+		self.total_cost = total_cost;
+		self.name = namegenerator.gen();
+		self.taped = False;
+
+class Land()
+		
+		color = ""
+		name = ""
+		supertype = "Basic"
+		rarity
+
+		def change_color(color):
+			self.color = color
+
+		def gen_color():
+			# the __none__ color is for colorless mana.
+
+			#Select the mana color
+			color_id = randint(0,5)
+			colors=["none","basic","white","blue","red","green"]
+			names=["Wastes","Plains","Island","Swamp","Mountain","Forest"]
+
+			self.Name = names[color_id]
+			self.color = colors[color_id]
+
+
+
+
+
+
