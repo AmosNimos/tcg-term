e9434011ed050d3f5d6747096ef388ca605a11a2
Amos Nimos <76786560+AmosNimos@users.noreply.github.com>
2021-07-30 13:42:46 -0400

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,7 +1,5 @@
-from random import choice
-
 ## Cards class
-class Creature:
+class Creature():
 	def __init__(self, power, taughness, cost, card_kind):
 		self.power = power;
 		self.taughness = taughness;
@@ -11,11 +9,10 @@ class Creature:
 		self.name = namegenerator.gen();
 		self.taped = False;
 
-class Land()
-		color = ""
-		name = ""
+class Land():
+		color = "none"
+		name = "Wastes"
 		supertype = "Basic"
-		rarity
 
 		def change_color(color):
 			self.color = color
@@ -25,7 +22,7 @@ class Land()
 
 			#Select the mana color
 			color_id = randint(0,5)
-			colors=["none","basic","white","blue","red","green"]
+			colors=["none","white","blue","black","red","green"]
 			names=["Wastes","Plains","Island","Swamp","Mountain","Forest"]
 
 			self.Name = names[color_id]
@@ -42,16 +39,23 @@ class Player:
 	mana_zone = [],
 	field_zone = [], 
 	graveyard = [],
-	def __init__(self, name, self_id, deck, health, hand_size):
+	def __init__(self, name, self_id, health, hand_size):
 		self.name = "Bob Smith",
 		self.self_id = self_id,
 		self.deck = deck,
 		self.hand = hand,
 		self.health = health,
 		
-class AI:
-	def __init__(self, name, self_id, deck, health, hand_size):
+	def gen_deck():
+		# This is just a temporary solution.
+		land_count = 30;
+		creature_count = 30;
+		
+		for x in lands_count:
+			self.deck.append(Land())
+		creature_cost = [1,0,0,0,0,0];
+		for x in creature_count:
+			self.deck.append(Creature(1,1,creature_cost))
 		
 class AI:
-	def __init__(self, name, self_id, deck, health, hand_size):
-		
+	def __init__(self, name, self_id, deck, health, hand_size):	
