efcb0b89742a05bb37690e868f27abe0fc08ac3a
Amos Nimos <76786560+AmosNimos@users.noreply.github.com>
2021-08-03 22:25:41 -0400

    Update test.py
    Fixed movement baby! oh yeah!

--- TEST/test.py
+++ TEST/test.py
@@ -2,6 +2,8 @@
 
 import classes
 import random
+import os
+
 # Vars
 ## Rules Variables
 deck_size = 60;
@@ -16,54 +18,67 @@ def initialisation():
 	return player
 
 def field():
-	# local variable
-	creatures_zone = "";
-	permanents_zone = "";
-	lands_zone = "";
-	hand = "";
-	
-	while true:
+	while True:
+		os.system('clear')
+		# local variable
+		creatures_zone = "";
+		permanents_zone = "";
+		lands_zone = "";
+		hand = "";
 		# here should be AI side of the field
 		# ---
 
 		# display Player side of the field & the cursor
-		for card in range(player.creatures_zone):
-			if player.cursor_x == card and player.cursor_y == 0:
+		for card in range(len(player.creatures_zone)):
+			if player.cursor_x == card and player.cursor_y == 4:
 				creatures_zone += str(player.symbol)
 			else:
 				creatures_zone += cursor_symbol;
 		print(creatures_zone)
 
 		for card in player.permanents_zone:
-			if player.cursor_x == card and player.cursor_y == 0:
+			if player.cursor_x == card and player.cursor_y == 3:
 				permanents_zone += str(card.symbol)
 			else:
 				permanents_zone += cursor_symbol;
 		print(permanents_zone)
 
 		for card in player.lands_zone:
-			if player.cursor_x == card and player.cursor_y == 0:
+			if player.cursor_x == card and player.cursor_y == 2:
 				lands_zone += str(card.symbol)
 			else:
 				lands_zone += cursor_symbol;
 		print(lands_zone)
 
-		for card in player.hand:
-			if player.cursor_x == card and player.cursor_y == 0:
-				hand += str(card.symbol)
+		for card in range(len(player.hand)):
+			if player.cursor_x == card and player.cursor_y == 1:
+				hand += cursor_symbol; 
 			else:
-				hand += cursor_symbol;
+				hand += str(player.hand[card].symbol)
 		print(hand+"["+str(len(player.hand))+"]")
 
 		if player.cursor_y == 0:
 			print("#["+str(len(player.deck))+"]")
 		else:
 			print(cursor_symbol+"["+str(len(player.deck))+"]")
+			
+		# Movement input
+		print("y:"+str(player.cursor_y)+" x:"+str(player.cursor_x))
 		movement = input("Cursor:")
 		if movement[0] == "y":
-			player.cursor_y += movement[1];
+			if isinstance(movement[1], int):
+				player.cursor_y += int(movement[1]);
+			elif movement[1] == "+":
+				player.cursor_y += int(movement[2]);
+			elif movement[1] == "-":
+				player.cursor_y -= int(movement[2]);
 		elif movement[0] == "x":
-			player.cursor_x += movement[1];
+			if isinstance(movement[1], int):
+				player.cursor_x += int(movement[1]);
+			elif movement[1] == "+":
+				player.cursor_x += int(movement[2]);
+			elif movement[1] == "-":
+				player.cursor_x -= int(movement[2]);
 	
 player = initialisation();
 field();
